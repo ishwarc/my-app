25edceb498843953d6076330a4a4cc72ed7cabbf
i c <ishwarc1620@gmail.com>
2017-12-14 15:38:33 -0700

    added reset button

--- src/index.js
+++ src/index.js
@@ -46,16 +46,29 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.reset = this.reset.bind(this);
     this.state = {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      stepNumber: 0,
+      xIsNext: true,
     };
   }
 
+  reset(){
+    this.setState({
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+
+    });
+  }
+
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -66,13 +79,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -104,6 +125,9 @@ class Game extends React.Component {
         <div className="game-info">
           <div>{status}</div>
           <ol>{moves}</ol>
+        </div>
+        <div>
+        <button onClick={this.reset} type="button">Reset</button>
         </div>
       </div>
     );
